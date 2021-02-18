cc9765b22ebdd7f1307938ecdff2b3bdae40c097
Arjun-NA <arjunna.mm@gmail.com>
2021-02-17 19:39:00 -0500

    scripts NNI

--- scripts/Hparam_tuning.sh
+++ scripts/Hparam_tuning.sh
@@ -2,14 +2,14 @@
 #####  Constructed by HPC everywhere #####
 #SBATCH --mail-user=arjuna@iu.edu
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=32
 #SBATCH --time=2-0:00:00
 #SBATCH --mem=600gb
 #SBATCH --partition=gpu
 #SBATCH --gpus v100:4
 #SBATCH --mail-type=FAIL,END
 #SBATCH --job-name=RESCAP
-#SBATCH --output=hparam_tuner_%j.txt
+#SBATCH --output=Hparam/hparam_tuner_%j.txt
 
 
 ######  Module commands #####
@@ -18,6 +18,6 @@ module load anaconda
 
 ######  Job commands go below this line #####
 source activate rescapnet
-cd /N/slate/arjuna/Software_Residual_Capsule_Network/
+cd /N/slate/arjuna/RCNX2/
 nnictl create --config config.yml
 nvidia-smi -l 20
